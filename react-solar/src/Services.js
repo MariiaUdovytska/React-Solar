8ad3471f06b9409a0d79c948319713d3c674fdb6
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-04-26 12:34:56 +0300

    add component Services

---
+++ react-solar/src/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './css/services.css';
+
+class Services extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render(){
+		return(
+			<section>
+			</section>
+		)
+	}
+}
+
+export default Services;
