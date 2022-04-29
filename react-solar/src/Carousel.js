890c7c8d7c643ba1151ffe3528fcb9cdc79dbcfb
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-04-29 12:32:53 +0300

    add skeleton for component Carousel and styles for btns

--- react-solar/src/Carousel.js
+++ react-solar/src/Carousel.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import arrowLeft from './img/arrow/arrowLeft.svg';
+import arrowRight from './img/arrow/arrowRight.svg';
 
 class Carousel extends React.Component {
 	constructor(props) {
@@ -28,16 +30,24 @@ class Carousel extends React.Component {
 		let itemsList= [];
 		for(let child= 0; child< this.props.children.length; child++)
 		{
-			itemsList.push(<div key={child} className='test-animation' style={{transform: value}}>{this.props.children[child]}</div>)
+			itemsList.push(<div key={child} className='carousel__content-item' style={{transform: value}}>{this.props.children[child]}</div>)
 		}
 		return(
-			<section>
-				<div className='test-d'>
-					{itemsList}
+			<div className='carousel'>
+				<div className='carousel__content'>
+					<div className='carousel__content-items'>
+						{itemsList}
+					</div>
 				</div>
-				<button type="button" className='test' onClick={this.leftButtonClick}>Left</button>
-				<button type="button" className='test' onClick={this.rigthButtonClick}>Right</button>
-			</section>
+				<div className='carousel__btns'>
+					<button type="button" className='carousel__btns-left' onClick={this.leftButtonClick}>
+						<img src={arrowLeft} alt="Arrow left" width={40}></img>
+					</button>
+					<button type="button" className='carousel__btns-right' onClick={this.rigthButtonClick}>
+						<img src={arrowRight} alt="Arrow right" width={40}></img>
+					</button>
+				</div>
+			</div>
 		)
 	}
 }
--- react-solar/src/GreenTariff.js
+++ react-solar/src/GreenTariff.js
@@ -11,10 +11,10 @@ class GreenTariff extends React.Component {
 	render(){
 		return(
 				<Carousel>
-					<div>1fvnasvna afvafnvn afvsfv</div>
-					<div>2fvnasvna afvafnvn afvsfv</div>
-					<div>3fvnasvna afvafnvn afvsfv</div>
-					<div>4fvnasvna afvafnvn afvsfv<br></br> rgarsgre</div>
+					<div>Цена, по которой домохозяйства могут продать электроэнергию, более чем в 10 раз превышает цену, по которой они ее покупают для своих нужд — 0,16 Евро/кВт (продажа).</div>
+					<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+					<div>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</div>
+					<div>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</div>
 				</Carousel>
 		)
 	}
