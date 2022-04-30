fcb7af77d08ed219b04f5f505c9186e55227dde6
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-04-30 19:04:12 +0300

    add styles for  component Greentariff

--- react-solar/src/GreenTariff.js
+++ react-solar/src/GreenTariff.js
@@ -20,10 +20,26 @@ class GreenTariff extends React.Component {
 					<div className='greenTariff__body-title titleM'>ЗЕЛЕНЫЙ ТАРИФ</div>
 					<div className='greenTariff__body-row'>
 						<div className='greenTariff__body-row-left'>
-							{/* <div className='greenTariff__body-row-left-corouselUp'> */}
-								<CarouselExternal/>
-								
-							{/* </div> */}
+							<CarouselExternal/>
+							<div className='greenTariff__body-row-left-inform'>
+								<div className='greenTariff__body-row-left-inform-item grid1'>
+									<h2>Гарантия выкупа</h2>
+									<p>Гарантированный выкуп электроэнергии государством - прописано в НКРЕ №447</p>
+								</div>
+								<div className='greenTariff__body-row-left-inform-item grid2'>
+									<h2>Долгосрочно</h2>
+									<p>это проект, поддерживаемый международными инвестиционными организациями</p>
+								</div>
+								<div className='greenTariff__body-row-left-inform-item grid3'>
+									<h2>Вид вложения</h2>
+									<p>Альтернатива банковским депозитам, окупаемость - от 16% до 18% годовых</p>
+								</div>
+								<div className='greenTariff__body-row-left-inform-item grid4'>
+									<h2>Высокая ставка</h2>
+									<p>Самая высокая тарифная ставка в европе - 0,16 евро</p>
+								</div>
+							</div>
+
 						</div>
 						
 						<div className='greenTariff__body-row-right'>
