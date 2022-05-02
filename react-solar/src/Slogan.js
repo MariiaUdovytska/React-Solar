1faf5502d05dcb599859b40b60091d6f06842d37
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-02 09:16:19 +0300

    create component Slogan

---
+++ react-solar/src/Slogan.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './css/slogan.css'
+
+class Slogan extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render(){
+
+		return(
+			<div className='slogan'>
+				
+			</div>
+		)
+	}
+}
+
+export default Slogan;
