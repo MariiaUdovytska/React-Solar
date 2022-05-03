41114d171deaa2bc6f7a2d0b265f2736fa6dffb4
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-03 22:35:29 +0300

    add styles for right component Clients

--- react-solar/src/Clients.js
+++ react-solar/src/Clients.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './css/clients.css';
 import './css/buttonBlack.css';
 import Carousel from './Carousel';
-// import SplashScreenBtn from './SplashScreenBtn';
 import Button from './Button';
+import clients1 from './img/clients/firstClients.jpg';
 
 class Clients extends React.Component {
 	constructor(props) {
@@ -14,19 +14,24 @@ class Clients extends React.Component {
 			<section className='clients'>
 				<div className='clients__body containerM'>
 					<div className='clients__title titleM'>НАШИ КЛИЕНТЫ</div>
-					<div className='clients__row'>
-						<Carousel>
-							<div></div>
-							<div></div>
-							<div></div>
-							<div></div>
-						</Carousel>
-						<div className='clients__right'>
-							<div className='clients__right-info'>
+					<div className='clients__body-row'>
+						<div className='clients__body-left'>
+							<Carousel>
+								<div>
+									<img width={182} height={51} src={clients1} alt="Clients1" />
+								</div>
+								<div></div>
+								<div></div>
+								<div></div>
+							</Carousel>
+						</div>
+						
+						<div className='clients__body-right'>
+							<div className='clients__body-right-info'>
 								<h2>Солнечная электростанция 30 кВт подключенная по Зеленому Тарифу</h2>
-								<div className='clients__right-info-p'>
+								<div className='clients__body-right-info-p'>
 									<p>Расположение: Харьковская обл., г. Харьков</p>
-									<p>Чистый доход за вычетом налогов с мая 2017 г. по май 2019 г. 13 900 $.</p>
+									<p>Чистый доход за вычетом налогов с мая 2017 г. по май 2019 г. 13&nbsp;900&nbsp;$.</p>
 									<p>Смонтирована: апрель 2017 г.</p>
 									<p>Срок службы: 25-30 лет</p>
 								</div>
