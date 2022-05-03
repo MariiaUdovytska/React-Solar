3eed0a768f1bb714f9603fe66d729e2f1d79a5d5
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-03 12:41:37 +0300

    add component Clients

---
+++ react-solar/src/Clients.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/clients.css';
+
+class Clients extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render(){
+		return(
+			<section className='clients'>
+				
+			</section>
+		)
+	}
+}
+
+export default Clients;
