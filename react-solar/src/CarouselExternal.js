13e756e0863a7fd31a87f45437d7395d08689047
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-05-30 21:08:28 +0300

    change cursor for carouselExternal

--- react-solar/src/CarouselExternal.js
+++ react-solar/src/CarouselExternal.js
@@ -23,9 +23,9 @@ class CarouselExternal extends React.Component {
 			<div className='carouselExternal'>
 				<div className='carouselExternal__body'>
 					<div className='carouselExternal__body-name'>
-						<span className={((this.state.move==0)?' carouselExternal__active':'')} onClick={()=>{this.clickLabel(0)}}>продажа</span>
-						<span className={((this.state.move==1)?' carouselExternal__active':'')} onClick={()=>{this.clickLabel(1)}}>закрепление</span>
-						<span className={((this.state.move==2)?' carouselExternal__active':'')} onClick={()=>{this.clickLabel(2)}}>ограничения</span>
+						<span className={((this.state.move==0)?' carouselExternal__active':'carouselExternal__noactive')} onClick={()=>{this.clickLabel(0)}}>продажа</span>
+						<span className={((this.state.move==1)?' carouselExternal__active':'carouselExternal__noactive')} onClick={()=>{this.clickLabel(1)}}>закрепление</span>
+						<span className={((this.state.move==2)?' carouselExternal__active':'carouselExternal__noactive')} onClick={()=>{this.clickLabel(2)}}>ограничения</span>
 					</div>
 					<div className='carouselExternal__body-content-items'>
 						<div className='carouselExternal__body-content-item' style={{transform: value}}><CarouselSale/></div>
